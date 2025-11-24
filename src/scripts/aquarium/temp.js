80223fc7234cbdaf345c70e89688f25b05c1f34e
illun0r <anton.kai.programm@gmail.com>
2025-11-24 13:52:23 +0100

    nichts

--- src/scripts/aquarium/temp.js
+++ src/scripts/aquarium/temp.js
@@ -1,38 +1,38 @@
-//Thermometer einblenden (Wasser-Temperatur).
-// function showThermometer() {
-//   Falls schon eins da ist → nichts machen
-//   if (document.getElementById("thermo")) return;
+/* // Thermometer einblenden (Wasser-Temperatur).
+function showThermometer() {
+  // Falls schon eins da ist → nichts machen
+  if (document.getElementById("thermo")) return;
 
-//   const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
-//   group.setAttribute("id", "thermo");
+  const group = document.createElementNS("http://www.w3.org/2000/svg", "g");
+  group.setAttribute("id", "thermo");
 
-//   Glasröhre
-//   const tube = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-//   tube.setAttribute("x", "760");
-//   tube.setAttribute("y", "100");
-//   tube.setAttribute("width", "12");
-//   tube.setAttribute("height", "300");
-//   tube.setAttribute("rx", "6");
-//   tube.setAttribute("fill", "rgba(255,255,255,0.2)");
-//   group.appendChild(tube);
+  // Glasröhre
+  const tube = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  tube.setAttribute("x", "760");
+  tube.setAttribute("y", "100");
+  tube.setAttribute("width", "12");
+  tube.setAttribute("height", "300");
+  tube.setAttribute("rx", "6");
+  tube.setAttribute("fill", "rgba(255,255,255,0.2)");
+  group.appendChild(tube);
 
-//   Quecksilber
-//   const mercury = document.createElementNS(
-//     "http://www.w3.org/2000/svg",
-//     "rect"
-//   );
-//   mercury.setAttribute("id", "mercury");
-//   mercury.setAttribute("x", "760");
-//   mercury.setAttribute("width", "12");
-//   mercury.setAttribute("rx", "6");
-//   mercury.setAttribute("fill", "#ef4444"); // rot
-//   group.appendChild(mercury);
+  // Quecksilber
+  const mercury = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "rect"
+  );
+  mercury.setAttribute("id", "mercury");
+  mercury.setAttribute("x", "760");
+  mercury.setAttribute("width", "12");
+  mercury.setAttribute("rx", "6");
+  mercury.setAttribute("fill", "#ef4444"); // rot
+  group.appendChild(mercury);
 
-//   svg.appendChild(group);
-//   updateThermometer();
-// }
+  svg.appendChild(group);
+  updateThermometer();
+  } */
 
-// Temperaturbalken aktualisieren
+/* Temperaturbalken aktualisieren */
 function updateThermometer() {
   const mercury = document.getElementById("mercury");
   if (!mercury) return;
